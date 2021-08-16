de5e914149cdc724c213f6fdebc9b470ce33a270
Julian Montero <71648137+JulianMGonzalez@users.noreply.github.com>
2021-08-16 18:03:11 -0500

    create spinner loading and create news component

---
+++ src/services/settings.js
@@ -0,0 +1,4 @@
+export const API_KEY_NEWS = '66f8be2afad5497f83cd037f1d15bb1b'
+export const API_URL_NEWS = 'https://newsapi.org/v2'
+export const API_COUNTRY_NEWS = 'co'
+export const API_CATEGORY_NEWS = 'health'
